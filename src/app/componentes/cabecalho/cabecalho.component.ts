5628cb45dfaf33d4f6602cae10cb3082ba068d2d
Rodolfo Queiroz <rodolfo.dqueiroz@gmail.com>
2022-11-15 17:08:44 -0300

    Adequações header e footer

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
-  styleUrls: ['./cabecalho.component.css']
+  styleUrls: ['./cabecalho.component.css'],
 })
 export class CabecalhoComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  direcionaParaHome() {
+    this.router.navigate(['/listarProdutos']);
   }
-
 }
